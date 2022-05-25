094fa8a0b1482fd7eaa9ed6cd06c65ff3394ab96
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-05-25 21:28:11 +0100

    feat: add command key display component

---
+++ src/components/CommandKeyDisplay.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+
+const DisplayWrapper = styled.div`
+  width: 250px;
+  height: 40px;
+
+  background-color: lightgrey;
+  display: flex;
+  flex-direction: row;
+`;
+
+const LeftWrapper = styled.div`
+`;
+
+const ClearKeyBox = styled.div`
+  width: 40px;
+  height: 40px;
+
+  box-sizing: border-box;
+  border: solid 2px white;
+  border-radius: 3px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const ClearKey = styled.span`
+  color: white;
+  font-size: 1.25em;
+`;
+
+const PressedKeyBox = styled.div`
+  width: 40px;
+  height: 40px;
+
+  box-sizing: border-box;
+  border: solid 2px white;
+  border-radius: 3px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  box-sizing: border-box;
+  background-color: white;
+  mix-blend-mode: screen;
+`;
+
+const PressedKey = styled.span`
+  color: black;
+  font-size: 1.25em;
+`;
+
+const RightWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Command = styled.span`
+  margin: 0;
+  margin-left: 8px;
+  color: white;
+  font-size: 1.25em;
+`;
+
+export const CommandKeyDisplay = ({ 
+  letter,
+  command,
+  pressed 
+}) => {
+  return (
+    <DisplayWrapper>
+      <LeftWrapper>
+        {pressed ?
+          <PressedKeyBox><PressedKey>{letter}</PressedKey></PressedKeyBox> :
+          <ClearKeyBox><ClearKey>{letter}</ClearKey></ClearKeyBox>
+        }
+      </LeftWrapper>
+      <RightWrapper><Command>{command}</Command></RightWrapper>
+    </DisplayWrapper>
+  );
+};
