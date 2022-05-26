5dbee2daa3585ba4502243f8236b1c08e3a48c26
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-05-26 16:56:29 +0100

    feat: add key detection for command key components

--- src/components/CommandKeyDisplay.js
+++ src/components/CommandKeyDisplay.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const DisplayWrapper = styled.div`
@@ -62,11 +63,41 @@ const Command = styled.span`
   font-size: 1.25em;
 `;
 
+// External Sol
+// https://usehooks.com/useKeyPress/
+function useKeyPress(targetKey) {
+  // State for keeping track of whether key is pressed
+  const [keyPressed, setKeyPressed] = useState(false);
+  // If pressed key is our target key then set to true
+  function downHandler({ key }) {
+    if (key === targetKey) {
+      setKeyPressed(true);
+    }
+  }
+  // If released key is our target key then set to false
+  const upHandler = ({ key }) => {
+    if (key === targetKey) {
+      setKeyPressed(false);
+    }
+  };
+  // Add event listeners
+  useEffect(() => {
+    window.addEventListener("keydown", downHandler);
+    window.addEventListener("keyup", upHandler);
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+      window.removeEventListener("keyup", upHandler);
+    };
+  }); // Empty array ensures that effect is only run on mount and unmount
+  return keyPressed;
+}
+
 export const CommandKeyDisplay = ({ 
   letter,
-  command,
-  pressed 
+  command
 }) => {
+  var pressed = useKeyPress(letter.toLowerCase());
   return (
     <DisplayWrapper>
       <LeftWrapper>
