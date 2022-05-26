705603be58652c4552c0f6e4e386162fee24a8a4
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-05-26 10:47:54 +0100

    feat: add connection display component

---
+++ src/components/ConnectionDisplay.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+
+const DisplayWrapper = styled.div`
+  margin: 0;
+  width: 600px;
+  background-color: lightgrey;
+  color: white;
+`;
+
+const CameraDisplay = styled.p`
+  margin:0;
+  margin-bottom: 8px;
+  font-size: 2.75em;
+`;
+
+const DeviceSpan = styled.span`
+  font-weight: bold;
+`;
+
+const CameraSpan = styled.span`
+`;
+
+const StatusDisplay = styled.p`
+  margin: 0;
+  font-size: 0.75em;
+  font-weight: bold;
+`;
+
+
+export const ConnectionDisplay = ({
+  device,
+  camera,
+  connected,
+  method,
+  time,
+  address
+}) => {
+  
+
+  return (
+    <DisplayWrapper>
+      <CameraDisplay>
+        <DeviceSpan>{device + " > "}</DeviceSpan>
+        <CameraSpan>{(camera ? camera : "NO FEED")}</CameraSpan>
+      </CameraDisplay>
+      { connected ?
+        <StatusDisplay>CONNECTED-{address}-{method}-{(time.toTimeString()).toUpperCase()} </StatusDisplay> :
+        <StatusDisplay>ESTABLISHING CONNECTION...</StatusDisplay>
+      }
+    </DisplayWrapper>
+  );
+};
+
+
