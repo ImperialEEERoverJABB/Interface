5cacec432cc4bce9087b488f2e66b3b616495e25
Ping-Sheng Wang <samuelpswang@gmail.com>
2022-06-10 14:45:26 +0100

    feat: assemble connections in main container

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,12 +1,18 @@
-import styled from 'styled-components';
+import { useEffect, useState } from "react";
+import styled from "styled-components";
 
-import { MissionClockDisplay } from '../components/MissionClockDisplay';
-import { SensorDisplay } from '../components/SensorDisplay';
-import { ConnectionDisplay } from '../components/ConnectionDisplay';
-import { ClockDisplay } from '../components/ClockDisplay';
-import { CommandKeyDisplay } from '../components/CommandKeyDisplay';
+import { useKeyPress } from "../utils/KeyPress.util"
 
-import DummyImage from '../img/dummy-img-mars.png';
+import { MissionClockDisplay } from "../components/MissionClockDisplay";
+import { SensorDisplay } from "../components/SensorDisplay";
+import { ConnectionDisplay } from "../components/ConnectionDisplay";
+import { ClockDisplay } from "../components/ClockDisplay";
+import { CommandKeyDisplay } from "../components/CommandKeyDisplay";
+import { RockModal } from "../components/RockModal";
+
+import DummyImage from "../img/dummy-img-mars.png";
+
+import { drive, reverse, left, right, end, sensors } from "../apis/HttpComms.api";
 
 const Container = styled.div`
   display: flex;
@@ -75,30 +81,135 @@ const Label = styled.p`
   color: white
 `
 
-const start = new Date();
+export const MainContainer = () => {
+  // display states
+  const [connection, setConnection] = useState(null);
+  const [start, setStart] = useState(null);
+  const [data, setData] = useState({});
+  // function states
+  const [intervalId, setIntervalId] = useState(null);
+  
+  // util functions
+  const startSensing = () => {
+    let inProgressFlag = false;
+    var sensorIntervalId = setInterval(async () => {
+      if (inProgressFlag) {
+        return;
+      }
+      inProgressFlag = true;
+      try {
+        // try connecting
+        let parsed = await sensors();
 
-export const MainContainer = ({
-  data
-}) => {
+        // if connection successful and connection not set
+        if (!connection) {
+          // set connection state
+          setConnection(true);
+          // set start time
+          let systemStart = new Date((new Date()).getTime() - parsed.time);
+          setStart(systemStart);
+        }
+
+        setData(parsed);
+      }
+      catch (e) {
+        setConnection(false);
+        setIntervalId(null);
+        clearInterval(sensorIntervalId);
+      }
+      inProgressFlag = false;
+    }, 1000);
+
+    setIntervalId(sensorIntervalId);
+  }
+
+  const stopSensing = () => {
+    clearInterval(intervalId);
+    setIntervalId(null);
+  }
+
+  // effect: repeat get sensor
+  // hook
+  useEffect(() => { 
+    startSensing();
+  }, 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  [])
+
+  // effect: key presses
+  // keypresses
+  let w = useKeyPress("w");
+  let a = useKeyPress("a");
+  let s = useKeyPress("s");
+  let d = useKeyPress("d");
+  let j = useKeyPress("j");
+  // command hook
+  useEffect(() => {
+    if ((w + a + s + d) > 1) {
+      console.log("nothing");
+    }
+    else if (w) {
+      drive();
+      console.log("drive()");
+    }
+    else if (a) {
+      left();
+      console.log("left()");
+    }
+    else if (s) {
+      reverse();
+      console.log("reverse()");
+    }
+    else if (d) {
+      right();
+      console.log("right()");
+    }
+    else {
+      end();
+      console.log("end()");
+    }
+  }, 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  [w, a, s, d]);
+  // sensor hook
+  useEffect(() => {
+    if (j) {
+      if (intervalId) {
+        stopSensing();
+        console.log("stopSensing()");
+      }
+      else {
+        startSensing();
+        console.log("startSensing()");
+      }
+    }
+  }, 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  [j])
+
+  // components
   return (
+    <>
+    <RockModal show={data["rock"]} text={`${data.rock}`} />
     <Container>
       <DisplayContainer>
         <Left>
           <MissionClockDisplay start={start}/>
           <SensorModule>
-            <SensorDisplayWrapper><SensorDisplay name="ACOUSTIC SIG" value="40.01" unit="KHZ"/></SensorDisplayWrapper>
-            <SensorDisplayWrapper><SensorDisplay name="RADIO SIG" value={undefined} unit="HZ"/></SensorDisplayWrapper>
-            <SensorDisplayWrapper><SensorDisplay name="INFRARED SIG" value="353.2" unit="HZ"/></SensorDisplayWrapper>
-            <SensorDisplayWrapper><SensorDisplay name="MAGNETIC FLD" value="01.00" unit="G"/></SensorDisplayWrapper>
+            <SensorDisplayWrapper><SensorDisplay name="ACOUSTIC SIG" value={data.acoustic} unit="DB"/></SensorDisplayWrapper>
+            <SensorDisplayWrapper><SensorDisplay name="RADIO SIG" value={data.radio} unit="HZ"/></SensorDisplayWrapper>
+            <SensorDisplayWrapper><SensorDisplay name="INFRARED SIG" value={data.infrared} unit="HZ"/></SensorDisplayWrapper>
+            <SensorDisplayWrapper><SensorDisplay name="MAGNETIC FLD" value={data.magnetic} unit="G"/></SensorDisplayWrapper>
             <Label>ENVIRONMENT</Label>
           </SensorModule>
-          <ConnectionDisplay device="EEEROVER" camera="FRONT" connected address="192.168.0.17" method="HTTP" time={start}/>
+          <ConnectionDisplay connected={connection} device="EEEROVER" address="192.168.0.17" method="HTTP" time={start}/>
         </Left>
         <Right>
           <ClockDisplay />
           <RoverModule>
-            <SensorDisplayWrapper><SensorDisplay name="SPEED" value="10.23" unit="M/S"/></SensorDisplayWrapper>
-            <SensorDisplayWrapper><SensorDisplay name="MODE" value="DRIVE" unit="D"/></SensorDisplayWrapper>
+            <SensorDisplayWrapper><SensorDisplay name="OUTPUT VOL" value="10.09" unit="V"/></SensorDisplayWrapper>
+            {/* <SensorDisplayWrapper><SensorDisplay name="BATTERY" value="12.34" unit="%"/></SensorDisplayWrapper> */}
+            <SensorDisplayWrapper><SensorDisplay name="MODE" value={data.mode} unit={(data.mode ? data.mode[0] : "N")}/></SensorDisplayWrapper>
             <Label>ROVER STATUS</Label>
           </RoverModule>
         </Right>
@@ -108,8 +219,9 @@ export const MainContainer = ({
         <CommandKeyDisplay letter="A" command="LEFT TURN"  />
         <CommandKeyDisplay letter="D" command="RIGHT TURN"  />
         <CommandKeyDisplay letter="S" command="REVERSE"  />
-        <CommandKeyDisplay letter="J" command="REFRESH SENSORS"  />
+        <CommandKeyDisplay letter="J" command="STOP/START SENSORS"  />
       </CommandContainer>
     </Container>
+    </>
   );
 };
